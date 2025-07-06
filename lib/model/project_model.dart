387eb4187da030c50d1d0166cf10f7f4ae8df61f
Huynh Quoc <quocb14005xx@ip-192-168-0-102.ap-southeast-1.compute.internal>
2025-07-06 15:20:27 +0700

    update projects

--- lib/model/project_model.dart
+++ lib/model/project_model.dart
@@ -5,16 +5,17 @@ class Project {
   final String? link;
   final String? githubLink;
 
-  Project(this.name, this.description, this.image, this.link, {this.githubLink});
+  Project(this.name, this.description, this.image, this.link,
+      {this.githubLink});
 }
 
 List<Project> projectList = [
-  Project(
-    'CRM Website',
-    'A CRM (Customer Relationship Management) website is a web-based platform designed to help businesses manage their interactions with customers, streamline processes, and improve overall customer relationships. It typically includes features such as contact management, sales tracking, lead generation, and customer support.',
-    'assets/images/crm.png',
-    null,
-  ),
+  // Project(
+  //   'CRM Website',
+  //   'A CRM (Customer Relationship Management) website is a web-based platform designed to help businesses manage their interactions with customers, streamline processes, and improve overall customer relationships. It typically includes features such as contact management, sales tracking, lead generation, and customer support.',
+  //   'assets/images/crm.png',
+  //   null,
+  // ),
   Project(
     'ViiV',
     'Connecting the world of travel through vlogs\nViiV, an all-in-one travel app just for you',
@@ -28,10 +29,14 @@ List<Project> projectList = [
     'https://apkpure.com/vn/해니-기업전용/com.bloomcompany.hanny',
   ),
   Project(
-    'AloxideSDK',
-    'description: While there are a lot of blockchain softwaresavailable and it\'s not uncommon to see a requirement to support variousblockchains, it still requires a significant amount of time to learn how towrite a smart contract just printing "Hello World" per blockchain. Aloxideprovides a pragmatic abstraction for various blockchain softwares includingCAN, EOS, ICON, and so on so that you can focus on business logic onsmart contracts without ties to specific blockchain natures. Also based onthe abstraction Aloxide offers useful tool-kits for dApp development such asAPI gateway and SDK.',
-    'assets/images/aloxide.png',
-    null,
-    githubLink: 'https://github.com/lecle/aloxide-sdk-java'
-  ),
+      'AloxideSDK',
+      'While there are a lot of blockchain softwares available and it\'s not uncommon to see a requirement to support various blockchains, it still requires a significant amount of time to learn how to write a smart contract just printing "Hello World" per blockchain. Aloxide provides a pragmatic abstraction for various blockchain softwares includingCAN, EOS, ICON, and so on so that you can focus on business logic on smart contracts without ties to specific blockchain natures. Also based on the abstraction Aloxide offers useful tool-kits for dApp development such asAPI gateway and SDK.',
+      'assets/images/aloxide.png',
+      null,
+      githubLink: 'https://github.com/lecle/aloxide-sdk-java'),
+  Project(
+      'App modular builder',
+      'Build modular app with ease',
+      'assets/images/modular_builder.png',
+      'https://lecle-app-modular-builder.web.app/')
 ];
